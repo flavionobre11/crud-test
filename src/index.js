625ad442783e11a5e5d1d5f999b8b99b73ff7c24
flavionobre11 <flavionobre11@gmail.com>
2021-01-20 21:44:42 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 
 const app = express()
 
 // setting body-parser
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+// setting cors
+app.use(cors())
 
 // passando app como parametro para os controllers
 // controllers sao automaticamente adicionados
